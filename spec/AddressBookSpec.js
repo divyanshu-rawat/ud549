689e77de0b07483918844310e9c42fdf5ce0ce89
divyanshu <divyanshu.r46956@gmail.com>
2017-06-11 17:28:52 +0530

    Finished writing sync test's in jasmine !

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,9 +1,23 @@
 
 describe('Address Book',function() {
+
+	var addressBook,thisContact;
+
+	beforeEach(function () {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	})
+
 	it('Should be able to add Contact',function() {
-		var addressBook = new AddressBook();
-		var thisContact = new Contact();
+		
 		addressBook.add_contact(thisContact);
 		expect(addressBook.get_contact(0)).toBe(thisContact);
 	});
+
+	it('Should be able to delete a Contact',function () {
+		        
+        addressBook.add_contact(thisContact);
+        addressBook.delete_contact(0);
+        expect(addressBook.get_contact(0)).not.toBeDefined();
+	})
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -5,8 +5,13 @@ function AddressBook(){
 
 AddressBook.prototype.add_contact = function(obj){
 	this.contact.push(obj);
+	console.log(this.contact);
 };
 
 AddressBook.prototype.get_contact = function(index){
 	return this.contact[index];
+};
+
+AddressBook.prototype.delete_contact = function(index){
+	this.contact.splice(index,1);
 };
